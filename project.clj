a113ebcde8ae86e475b639ba32376db9057ea63a
Nathan Clark <Nathan.Clark@nasa.gov>
2015-12-15 16:26:33 -0500

    Found this lying around.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/tokenshift/sprint-poker"
   :license {:url "https://github.com/tokenshift/sprint-poker/blob/master/LICENSE"}
   :dependencies [[compojure "1.3.2"]
+                 [environ "1.0.0"]
                  [org.clojure/algo.generic "0.1.2"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
