e3b8a31ddd635b9ab8388eb33268b9cfa0773f4a
Nathan Clark <Nathan.Clark@nasa.gov>
2016-08-05 14:32:23 -0400

    Adding leiningen version to project.clj.

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.clojure/data.json "0.2.5"]
                  [ring/ring-core "1.3.2"]
                  [ring-server "0.4.0"]]
+  :min-lein-version "2.0.0"
   :main sprint-poker.core
   :uberjar-name "sprint-poker-standalone.jar"
   :aot [sprint-poker.core])
